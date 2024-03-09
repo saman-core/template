0f34419d572a56d6f72696a61a88ce29564d957c
Hernael <hperer@movealong.tech>
2024-03-09 12:20:15 -0400

    update log

--- src/main/java/io/samancore/TemplateApi.java
+++ src/main/java/io/samancore/TemplateApi.java
@@ -5,9 +5,11 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 
 import io.smallrye.mutiny.Uni;
+import org.jboss.logging.Logger;
 
 @Path("/templates")
 public class TemplateApi {
+    private static final Logger log = Logger.getLogger(TemplateApi.class);
 
     @Inject
     TemplateService service;
@@ -16,6 +18,7 @@ public class TemplateApi {
     @RolesAllowed({"admin"})
     @Path("/{product}/{template}")
     public Uni<Void> create(@PathParam("product") String product, @PathParam("template") String template, Object templateForm) {
+        log.debugf("TemplateApi.create %s/%s", product, template);
         return service.setValue(product, template, templateForm);
     }
 
@@ -23,6 +26,7 @@ public class TemplateApi {
     @RolesAllowed({"admin"})
     @Path("/{product}/{template}")
     public Uni<Object> get(@PathParam("product") String product, @PathParam("template") String template) {
+        log.debugf("TemplateApi.get %s/%s", product, template);
         return service.getValue(product, template);
     }
 
@@ -30,6 +34,7 @@ public class TemplateApi {
     @RolesAllowed({"admin"})
     @Path("/{product}/{template}")
     public Uni<Void> delete(@PathParam("product") String product, @PathParam("template") String template) {
+        log.debugf("TemplateApi.delete %s/%s", product, template);
         return service.delValue(product, template);
     }
 }
